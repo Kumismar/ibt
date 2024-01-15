919b1e524271e0d55cfb18439214af909d39e6c6
Kumismar <xkouma02@vutbr.cz>
2024-01-15 23:27:50 +0100

    fix: return RowAccessor by value

--- src/ll_table.cpp
+++ src/ll_table.cpp
@@ -1,13 +1,14 @@
 #include "headers/ll_table.hpp"
 #include "headers/nonterminal.hpp"
 #include "headers/nonterminal_type.hpp"
+#include "headers/token.hpp"
 
 const TableIndex& RowAccessor::operator[](Token& token) const
 {
     return this->row[(unsigned)(token.GetType())];
 }
 
-const RowAccessor& LLTable::operator[](Nonterminal& nonterminal) const
+const RowAccessor LLTable::operator[](Nonterminal& nonterminal) const
 {
     return RowAccessor(this->table[(unsigned)(nonterminal.GetType())]);
 }
