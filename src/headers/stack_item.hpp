51eb217d0fbf2053b7ecbf4de5fbecc6b1d06108
Kumismar <xkouma02@vutbr.cz>
2024-01-19 14:53:35 +0100

    fix: add type to stackitem
    token/nonterminal

--- src/headers/stack_item.hpp
+++ src/headers/stack_item.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
+typedef enum
+{
+    Token_t,
+    Nonterminal_t
+} ItemType;
+
 class StackItem
 {
+private:
+    ItemType type;
+
 public:
     virtual ~StackItem()
     {
     }
+
+    ItemType GetType();
 };
---
+++ src/stack_item.cpp
@@ -0,0 +1,6 @@
+#include "stack_item.hpp"
+
+ItemType StackItem::GetType()
+{
+    return this->type;
+}
