d250977ae3c2e2cc53a69f31c122f537db7cd0ea
Kumismar <xkouma02@vutbr.cz>
2024-01-19 14:53:54 +0100

    feat: operator != overload for tableindex

--- src/headers/table_index.hpp
+++ src/headers/table_index.hpp
@@ -3,4 +3,9 @@
 typedef struct tableIndex {
     unsigned grammarNumber;
     unsigned ruleNumber;
+
+    bool operator!=(const struct tableIndex& other) const
+    {
+        return ((this->grammarNumber == other.grammarNumber) && (this->ruleNumber == other.ruleNumber));
+    }
 } TableIndex;
