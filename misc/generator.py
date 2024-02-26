a8b816de2b8e1a712cfb4f63bd8b06f3e90034af
Ondřej <xkouma02@vutbr.cz>
2024-02-26 21:13:14 +0100

    chore: ll table generator (from txt) to vector

---
+++ misc/generator.py
@@ -0,0 +1,41 @@
+nonterminals = [
+    "program      ",
+    "statementList",
+    "statement    ",
+    "if2          ",
+    "declOrExp    ",
+    "returnExp    ",
+    "functionDef  ",
+    "params       ",
+    "params2      ",
+    "expression   ",
+    "args         ",
+    "args2        ",
+    "codeBlock    ",
+    "statements   ",
+    "volType      ",
+    "type         "
+]
+
+with open("lltable.txt", 'r') as f1:
+    with open("vector.txt", 'w') as f2:
+        f2.write(
+            "const std::vector<std::vector<LLTableIndex>> LLTable::table = {\n")
+        lines = f1.readlines()
+        f2.write(f"//\t\t\t\t\t{lines[0]}")
+        for i in range(1, len(lines)):
+            f2.write(f"/* {nonterminals[i-1]} */")
+            f2.write("\t{ ")
+            words = lines[i].split('\t')
+            for word in words:
+                end = False
+                if word.endswith("\n"):
+                    word = word.rstrip("\n")
+                    end = True
+                x, y = map(int, word.strip("()").split(','))
+                if end:
+                    f2.write(f"{{ {x}, {y} }} ")
+                else:
+                    f2.write(f"{{ {x}, {y} }}, ")
+            f2.write("},\n")
+        f2.write("};")
