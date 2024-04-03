3bbe4b2c41ad03e682728a2a773b3423e61f2219
Ondřej <xkouma02@vutbr.cz>
2024-04-03 18:33:41 +0200

    fix: add function definition to statements

---
+++ src/ast/statements/function_definition.hpp
@@ -0,0 +1,29 @@
+/**
+ * @ Author: Ondřej Koumar
+ * @ Email: xkouma02@stud.fit.vutbr.cz
+ * @ Create Time: 2024-04-03 18:30
+ * @ Modified time: 2024-04-03 18:33
+ */
+
+#pragma once
+
+#include "statement.hpp"
+#include "statement_list.hpp"
+#include "token.hpp"
+#include <string>
+#include <vector>
+
+class Parameter
+{
+private:
+    std::string name;
+    DataType type;
+};
+
+class FunctionDefinition : public Statement
+{
+private:
+    std::string name;
+    std::vector<Parameter*> params;
+    StatementList* body = nullptr;
+};
