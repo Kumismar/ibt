1a4db2370ac71f250280184a1452e163fc8a8c52
Ondřej <xkouma02@vutbr.cz>
2024-05-05 12:52:45 +0200

    doc: add while loop doxygen comments

--- src/ast/statements/while_loop.hpp
+++ src/ast/statements/while_loop.hpp
@@ -17,12 +17,41 @@
 class WhileLoop : public Statement
 {
 private:
+    /**
+     * @brief The condition of the while loop.
+     */
     Expression* condition = nullptr;
+
+    /**
+     * @brief The sequence of statements (body) of the loop.
+     */
     StatementList* body = nullptr;
 
 public:
+    /**
+     * @brief A constructor for the loop. Sets node type.
+     */
     WhileLoop();
+
+    /**
+     * @brief A destructor of the while loop.
+     */
     ~WhileLoop() override;
-    void PrintTree(std::ofstream& file, int& id, int parentId) override;
+
+    /**
+     * @brief Print information about the node to the output .dot file.
+     * @param file The output file.
+     * @param id The id of the node.
+     * @param parentId The id of the parent node.
+     */
     void LinkNode(ASTNode* node, Nonterminal& nt) override;
+
+    /**
+     * @brief Link a node to the AST.
+     * @details This method is called on current AST context.
+     * The AST nodes are linked to each other.
+     * @param node The node to link.
+     * @param nt The nonterminal to link.
+     */
+    void PrintTree(std::ofstream& file, int& id, int parentId) override;
 };
